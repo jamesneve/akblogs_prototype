e42fdd8457c58839e75379a274070636914a109b
jamesneve <jamesoneve@gmail.com>
2013-02-25 08:51:22 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FieryHoop::Application.config.secret_token = 'eadd64138fdcbb1c9cb8585a6c4dfcf8ec982aa2b247a4cb9e0aa7089341afd596dcb59e25d5546fc39b1ddd873de7095a26d3d911483d01da9895a89cf36d55'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FieryHoop::Application.config.session_store :cookie_store, key: '_fiery_hoop_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FieryHoop::Application.config.session_store :active_record_store
